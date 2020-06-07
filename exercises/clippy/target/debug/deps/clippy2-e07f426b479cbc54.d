d48a3f26f1f6ded3c5c3635d3a567ab37d8b7a1e
justinsorensen2dotnet <>
2020-06-07 18:00:02 -0400

    completed clippy2

---
+++ exercises/clippy/target/debug/deps/clippy2-e07f426b479cbc54.d
@@ -0,0 +1,5 @@
+/home/justin/rustlings/exercises/clippy/target/debug/deps/clippy2-e07f426b479cbc54.rmeta: clippy2.rs
+
+/home/justin/rustlings/exercises/clippy/target/debug/deps/clippy2-e07f426b479cbc54.d: clippy2.rs
+
+clippy2.rs:
