67f6d319325ac4562c5009cf1bcbcb95d7fcabf9
justinsorensen2dotnet <>
2020-06-08 22:52:17 -0400

    completed enums

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,11 +1,13 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Move{ x: u8, y: u8 },
+    Echo(String),
+    ChangeColor(u32, u32, u32),
+    Quit,
 }
 
 impl Message {
