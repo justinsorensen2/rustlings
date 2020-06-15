c4eb87ec2e672be8f40a740747a835253bd01789
justinsorensen2dotnet <>
2020-06-15 00:22:48 -0400

    completed test4

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,7 +5,15 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
+
+#[macro_use]
+mod macros {
+    macro_rules! my_macro {
+        ($val:expr) => {
+         format!("Hello {}", $val);
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
